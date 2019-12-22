6bd097109dada5153178b436bcd550e65935a64f
Jolanta Jasiulionyte <klpdjolanta@gmail.com>
2019-12-22 21:48:31 +0200

    WIP on the notes app

---
+++ App.js
@@ -0,0 +1,110 @@
+import {COLOURS, FONT, TODO_COLOUR} from './Const';
+import React, {useEffect, useState} from 'react';
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {fontBase, st} from './Style';
+
+const App = () => {
+  const [value, setValue] = useState('');
+  const [hue, setHue] = useState(TODO_COLOUR.pink);
+  const [todo, setTodo] = useState([
+    {to: 'go shoppong for christmas', done: true, i: 0, priority: true},
+    {to: 'floss for chrismtas eve', done: true, i: 1, priority: false},
+    {
+      to: 'write to fam and friends',
+      done: false,
+      i: 2,
+      priority: true,
+    },
+  ]);
+
+  const changeHue = () => {
+    const {yellow, pink, blue} = TODO_COLOUR;
+    if (hue === pink) setHue(yellow);
+    if (hue === yellow) setHue(blue);
+    if (hue === blue) setHue(pink);
+  };
+
+  return (
+    <>
+      <View style={{...st.strip, backgroundColor: hue.strip}}>
+        <View style={st.featureStrip}>
+          <TouchableOpacity onPress={() => changeHue()}>
+            <View
+              style={{
+                ...st.colorChange,
+                backgroundColor: hue.light,
+                borderColor: hue.light,
+              }}
+            />
+          </TouchableOpacity>
+          <Text style={st.stats}>
+            {`${todo.filter(({done}) => done).length} / ${todo.length}`}
+          </Text>
+        </View>
+      </View>
+
+      <View style={{...st.page, backgroundColor: hue.main}}>
+        <TextInput
+          enablesReturnKeyAutomatically
+          style={{...st.input, backgroundColor: hue.strip}}
+          clearButtonMode="always"
+          autoCapitalize="sentences"
+          maxLength={50}
+          placeholder={'✔️ ToDo: '}
+          placeholderTextColor={COLOURS.MARKS}
+          onChangeText={value => setValue(value)}
+          returnKeyType="done"
+          value={value}
+          onSubmitEditing={() => {
+            let newTodo = {i: todo.length, done: false, to: value};
+            setTodo([...todo, newTodo]);
+            setValue('');
+          }}
+        />
+
+        <ScrollView style={st.todoSection}>
+          {todo
+            .map(line => {
+              const {to, done, i, priority} = line;
+              return (
+                <View style={st.todo} key={i}>
+                  <Text
+                    style={{
+                      ...fontBase,
+                      textDecorationLine: done ? 'line-through' : null,
+                      textDecorationStyle: done ? 'solid' : null,
+                      color: done ? COLOURS.MARKS : 'black',
+                      fontSize: done ? FONT.size - 2 : FONT.size,
+                      fontWeight: priority
+                        ? String(Number(FONT.weight) + 400)
+                        : FONT.weight,
+                    }}
+                    onPress={() => {
+                      let newList = [...todo];
+                      newList[i].done = !done;
+                      setTodo(newList);
+                    }}
+                    onLongPress={() => {
+                      let newList = [...todo];
+                      newList[i].priority = !priority;
+                      setTodo(newList);
+                    }}>
+                    - {to}
+                  </Text>
+                </View>
+              );
+            })
+            .reverse()}
+        </ScrollView>
+      </View>
+    </>
+  );
+};
+
+export default App;
---
+++ Style.js
@@ -0,0 +1,66 @@
+import {COLOURS, FONT} from './Const';
+
+export const fontBase = {
+  fontFamily: FONT.family,
+  fontWeight: FONT.weight,
+  letterSpacing: FONT.spacing,
+};
+
+export const st = {
+  page: {
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+  },
+  input: {
+    ...fontBase,
+    fontSize: FONT.size,
+    height: 40,
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    borderRadius: 10,
+    width: '90%',
+    marginTop: '10%',
+    margin: 20,
+    padding: 5,
+    paddingHorizontal: 20,
+  },
+  strip: {
+    height: 100,
+    width: '100%',
+  },
+  todo: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    margin: 4,
+  },
+  todoSection: {
+    marginHorizontal: 30,
+    marginVertical: 10,
+  },
+  colorChange: {
+    width: 30,
+    height: 30,
+    borderRadius: 20,
+    borderWidth: 2,
+    marginLeft: 30,
+    shadowOpacity: 0.3,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 3,
+    shadowColor: 'black',
+  },
+  featureStrip: {
+    display: 'flex',
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    height: 80,
+  },
+  stats: {
+    ...fontBase,
+    color: COLOURS.MARKS,
+    fontSize: FONT.size - 1,
+  },
+};
